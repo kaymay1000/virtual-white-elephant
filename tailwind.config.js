633a0f8d4878f10b004dfec52afaac3c06fb48e3
Kaylee Alvarado <kaylee@assembleinc.com>
2021-12-06 00:59:09 -0800

    add tailwind to project per CRA-specific installation guide on TW's site

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  // configure Tailwind to remove unused styles in production
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
