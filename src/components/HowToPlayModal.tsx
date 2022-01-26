e0a479ea1f9a3fa82e90376397b19ef136d40461
Kaylee Alvarado <kaylee@assembleinc.com>
2022-01-25 17:21:28 -0800

    add bullets to how to play modal content

--- src/components/HowToPlayModal.tsx
+++ src/components/HowToPlayModal.tsx
@@ -26,15 +26,15 @@ const HowToPlayModal = ({
           <div className="px-4 pb-4">
             <Dialog.Title className="text-center font-bold">How To Play</Dialog.Title>
             <div className="py-2">
-              <ul>
-                <li>Designate one participant to be the Host</li>
-                <li>Each Participant places their gift in view of their webcam</li>
-                <li>Each Participant "draws" a number at random</li>
-                <li>Participant #1 chooses which gift they'd like to "open", and has the Participant who brought that gift unveil it on camera for all to see</li>
-                <li>Participant #2 has two options: choose another gift to open, or steal Participant #1's gift</li>
-                <li>If your gift is stolen, you may either choose a new gift to open or steal another gift</li>
-                <li>A gift can only be stolen twice after its initial opening-- after two steals, the gift "dies" and remains with its third/final owner</li>
-                <li>Gameplay continues sequentially until each Participant has gotten a chance to either open a new gift, or steal an existing one</li>
+              <ul className="list-disc">
+                <li>Designate one player to be the host, who will be in charge of updating VWE throughout the exchange.</li>
+                <li>Each player places their gift in view of their webcam.</li>
+                <li>Each player "draws" a number at random (VWE will handle this for you).</li>
+                <li>Player #1 chooses which gift he or she would like to "open", and has the player who brought that gift unveil it on camera for all to see.</li>
+                <li>Player #2 has two options: choose another gift to open, or steal Player #1's gift.</li>
+                <li>If your gift is stolen, you may either choose a new gift to open or steal another gift.</li>
+                <li>A gift can only be stolen twice after its initial opening- after two steals, the gift "dies" and remains with its third/final owner.</li>
+                <li>Gameplay continues sequentially until each player has gotten a chance to either open a new gift, or steal an existing one.</li>
               </ul>
             </div>
           </div>
