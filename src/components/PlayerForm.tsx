c5472c7bcdd2f3fcddcd2de7b25e61945130b80a
Kaylee Alvarado <kaylee@assembleinc.com>
2022-01-27 03:12:57 -0800

    field array working; console log returning expected players array on submit

--- src/components/PlayerForm.tsx
+++ src/components/PlayerForm.tsx
@@ -1,26 +1,24 @@
-import { Formik, Form, Field, FieldArray, FormikHelpers, FormikProps, useFormik } from 'formik';
+import { Formik, Form, Field, FieldArray, FormikHelpers, FormikProps, FormikProvider, useFormik } from 'formik';
 import Icon from './Icon';
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom';
 
 interface PlayerFormValues {
-  // players: Array<{ name: string }>
-  firstName: string;
+  players: Array<{ name: string }>
 }
 
 const PlayerForm = () => {
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const initialValues: PlayerFormValues = {
-    // players: [{name: ''}]
-    firstName: ''
+    players: [{name: ''}]
   }
 
+  {/* https://formik.org/docs/api/useFormik */}
   const formik = useFormik({
     initialValues: initialValues,
     onSubmit: values => {
-      alert(values);
       console.log(values);
     }
   });
@@ -31,59 +29,56 @@ const PlayerForm = () => {
       <form
         onSubmit={formik.handleSubmit}
       >
-        {/* https://formik.org/docs/examples/field-arrays */}
-        {/* <FieldArray name="players">
-          {arrayHelpers => (
-            <div className="my-4">
-              {formik.values.players.length > 0 && (
-                formik.values.players.map((player, index) => (
-                  <div key={index} className="flex justify-center">
-                    <label 
-                      htmlFor={`players[${index}].name`}
-                      className="text-sm md:text-base px-2 flex items-center"
-                    >
-                      Name
-                    </label>
-                    <input 
-                      id={`players[${index}].name`}
-                      name={`players[${index}].name`}
-                      onChange={formik.handleChange}
-                      value={formik.values.players[index].name}
-                      className="border-solid border-2 border-black px-1 my-2 w-40 md:w-auto"
-                    />
-          
-                    <button
-                      type="button"
-                      onClick={() => arrayHelpers.remove(index)}
-                      className="px-2 text-sm md:text-base"
-                    >
-                      <Icon iconName={faTrash}></Icon>
-                    </button>
-                  </div>
-                ))
-              )}
-              <button 
-                type="button" 
-                onClick={() => {arrayHelpers.push({name: ''});}}
-                className="border-solid border-2 border-black px-2 rounded hover:bg-green-400"
-              >
-                Add Player
-              </button>
-            </div>
-          )}
-        </FieldArray> */}
+        {/* Field Array doesn't play nice with useFormik hook, so have to wrap it in FormikProvider in order to pass Formik props to it */}
+        {/* https://stackoverflow.com/questions/59980094/can-i-use-fieldarray-alongside-useformik-hook */}
+        <FormikProvider value={formik}>
+          {/* https://formik.org/docs/examples/field-arrays */}
+          <FieldArray name="players">
+            {arrayHelpers => (
+              <div className="my-4">
+                {formik.values.players.length > 0 && (
+                  formik.values.players.map((player, index) => (
+                    <div key={index} className="flex justify-center">
+                      <label 
+                        htmlFor={`players[${index}].name`}
+                        className="text-sm md:text-base px-2 flex items-center"
+                      >
+                        Name
+                      </label>
+                      <input 
+                        id={`players[${index}].name`}
+                        name={`players[${index}].name`}
+                        onChange={formik.handleChange}
+                        value={formik.values.players[index].name}
+                        className="border-solid border-2 border-black px-1 my-2 w-40 md:w-auto"
+                      />
+            
+                      <button
+                        type="button"
+                        onClick={() => arrayHelpers.remove(index)}
+                        className="px-2 text-sm md:text-base"
+                      >
+                        <Icon iconName={faTrash}></Icon>
+                      </button>
+                    </div>
+                  ))
+                )}
+                <button 
+                  type="button" 
+                  onClick={() => {arrayHelpers.push({name: ''});}}
+                  className="border-solid border-2 border-black px-2 rounded hover:bg-green-400"
+                >
+                  Add Player
+                </button>
+              </div>
+            )}
+          </FieldArray>
+        </FormikProvider>
 
-        <label htmlFor="firstName">First Name</label>
-        <input
-          id="firstName"
-          name="firstName"
-          onChange={formik.handleChange}
-          value={formik.values.firstName}
-        />
-
-        {/* <div className="flex justify-center md:justify-end mt-2">
+        <div className="flex justify-center md:justify-end mt-2">
           <p>Total Players: {`${formik.values.players.length}`}</p>
-        </div> */}
+        </div>
+
         <div className="flex justify-center md:justify-end mt-2">
           <button 
             type="submit"
